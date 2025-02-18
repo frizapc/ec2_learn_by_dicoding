1919f9cff25e60652f8bb5f497e4ad884e01d053
frizapc <frizamuhammad99@gmail.com>
2025-02-18 21:00:21 +0700

    Add rabbitmq service

--- src/server.js
+++ src/server.js
@@ -27,6 +27,10 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
 const TokenManager = require('./tokenize/TokenManager');
 
 const init = async () => {
@@ -99,6 +103,13 @@ const init = async () => {
         collaborationsService,
         notesService,
         validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        validator: ExportsValidator,
       },
     },
   ]);
